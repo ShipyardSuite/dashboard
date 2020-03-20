c970b38b849119c7d7d30fd7a9693431adc550a8
Frederik Reich <frederikreich@googlemail.com>
2020-03-20 18:54:25 +0100

    Add layout

--- app/containers/Home/Home.js
+++ app/containers/Home/Home.js
@@ -2,16 +2,63 @@
 
 import React, { Component } from 'react';
 
+import { Segment, Loader, Dimmer } from 'semantic-ui-react';
+
+import { Layout } from './../../components';
+import { getFromStorage } from './../../utils/storage';
+
 class Home extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			isLoading: true,
+			token: '',
+			userData: []
+		};
+	}
+
+	componentDidMount() {
+		const obj = getFromStorage('botany-bay');
+
+		if (obj && obj.token !== '') {
+		} else {
+			window.location.replace('http://localhost:8080/auth/login');
+		}
+
+		if (obj && obj.token) {
+			const { token } = obj;
+
+			fetch('/auth/api/token?id=' + token).then((res) => res.json()).then((json) => {
+				if (json.success) {
+					console.log(json);
+					this.setState({
+						token,
+						isLoading: false
+					});
+				} else {
+					this.setState({
+						isLoading: false,
+						userData: []
+					});
+				}
+			});
+		} else {
+			this.setState({
+				isLoading: false
+			});
+		}
 	}
 
 	render() {
-		return <div>Dashboard...</div>;
+		const { isLoading } = this.state;
+
+		return <Layout isLoading={isLoading}>test</Layout>;
 	}
 }
 
 export default Home;
+
+/*
+
+*/
