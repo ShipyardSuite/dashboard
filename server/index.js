d4d7198e9f5cc8205e90371bf0672620736d750f
Frederik Reich <frederikreich@googlemail.com>
2020-04-29 15:08:26 +0200

    Add logger files

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const winston = require('winston');
+const Redis = require('winston-redis');
 
 /**
  * Main application class.
@@ -14,6 +16,7 @@ class App {
 		this.app = express();
 		this.serviceName = process.env.SERVICE_NAME || 'default';
 		this.servicePort = process.env.SERVICE_PORT || 3000;
+		this.logger;
 	}
 
 	/**
@@ -21,10 +24,40 @@ class App {
 	 * @method init
 	 */
 	init() {
+		this.configLogger();
 		this.config();
 		this.apiRoutes();
 		this.reactRoutes();
 		this.start();
+	}
+
+	/**
+	 * Configure Redis-Logger.
+	 * @method configLogger
+	 */
+	configLogger() {
+		this.logger = winston.createLogger({
+			format: winston.format.timestamp(),
+			defaultMeta: { service: process.env.SERVICE_NAME },
+			transports: [
+				new winston.transports.Console({
+					format: winston.format.combine(
+						winston.format.timestamp({ format: 'YYYY-MM-DD hh:mm:ss a' }),
+						winston.format.colorize(),
+						winston.format.simple(),
+						winston.format.printf(
+							(info) => `${info.timestamp} | ${info.level} | ${info.service} | ${info.message}`
+						)
+					)
+				}),
+				new Redis({
+					host: 'redis',
+					port: 6379,
+					container: 'logs',
+					expire: 7 * 24 * 60 * 60
+				})
+			]
+		});
 	}
 
 	/**
@@ -45,7 +78,7 @@ class App {
 	apiRoutes() {
 		fs.readdirSync(__dirname + '/api/').forEach((file, i, allRoutes) => {
 			if (allRoutes.length > 0) {
-				require(`./api/${file.substr(0, file.indexOf('.'))}`)(this.app, this.serviceName);
+				require(`./api/${file.substr(0, file.indexOf('.'))}`)(this.app, this.logger, this.serviceName);
 			}
 		});
 	}
@@ -69,10 +102,7 @@ class App {
 	 */
 	start() {
 		this.app.listen(this.servicePort, () => {
-			console.log(
-				`Service ${this.serviceName} listening on port ${this.servicePort} - http://localhost:${this
-					.servicePort}/${this.serviceName}/`
-			);
+			this.logger.info(`Service "${this.serviceName}" listening on port ${this.servicePort}`);
 		});
 	}
 }
